d82301c65b493f5eb11abb87f970362e8e030535
Andres Giraldo <andresganc@gmail.com>
2021-07-05 15:08:54 -0500

    New : Exercises AJAX

---
+++ exercises/exercises-ts/src/components/tag/tag-wc.js
@@ -0,0 +1,26 @@
+
+class TagWc extends HTMLElement {
+    constructor() {
+        super();
+        //implementation
+    }
+
+    connectedCallback() {
+        //implementation
+    }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+
+}
+
+window.customElements.define('tag-wc', TagWc);
