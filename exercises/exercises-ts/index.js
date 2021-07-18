9b7275a76e94e7ba9795e072e0c94d9c85ab53f7
Andres Giraldo <andresganc@gmail.com>
2021-07-18 11:08:04 -0500

    Mod: Structure Exercises TS

--- exercises/exercises-ts/index.js
+++ exercises/exercises-ts/index.js
@@ -1,3 +1,6 @@
+// Menus
+import './src/components/menus/menu-basic-01-wc'
+import './src/components/menus/menu-basic-vertical-01-wc'
 
 // Basic Exercises
 import './src/1-basic-exercises/hola-mundo-basic'
