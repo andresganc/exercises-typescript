afc56c5573c18332d9abfe998c664d7de202e415
Andres Giraldo <andresganc@gmail.com>
2021-07-20 09:19:01 -0500

    Mod: Exercises Basic TypeScript

--- exercises/exercises-ts/src/1-basic-exercises/01-length-of-a-chain.ts
+++ exercises/exercises-ts/src/1-basic-exercises/01-length-of-a-chain.ts
@@ -2,6 +2,6 @@
 
 
 function lengtchain() {
-    let InputText: string = document.querySelector('#InputText').value
+    let InputText: any = document.querySelector('#InputText').value;
     return window.alert('The lenght of the chain is: ' + InputText.length)
 }
