b1dcf699ec4fb8f6a51cf251bee70553a10968ec
Andres Giraldo <andresganc@gmail.com>
2021-08-10 09:34:09 -0500

    Mod: Change dependency nc web-components to - nc web-components-base

--- exercises/exercises-ts/index.js
+++ exercises/exercises-ts/index.js
@@ -10,5 +10,5 @@ import './src/1-basic-exercises/hola-mundo-function'
 // NC Web Components
 //import '@newsoft-computer/web-components/dist/collection/components/nc-button/nc-button'
 //ºimport '@newsoft-computer/web-components/dist/stencil/index.esm'
-import './node_modules/@newsoft-computer/web-components/dist/esm/stencil'
+import './node_modules/@newsoft-computer/web-components-base/dist/esm/stencil'
 
